2eaee220131856363a8b74a3d95373f2906cc3ed
originilala <jonathan.dewsaran@gmail.com>
2022-07-12 17:05:42 +0200

    Finish assignment three react

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+
+    return (
+        <>
+            <h1>Home pagina</h1>
+            <h2>Welkom op de hoofdpagina</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dignissimos ducimus eos, esse est eum
+                expedita, explicabo porro quas sequi sit unde voluptas voluptates. Blanditiis cupiditate deserunt dolor
+                est facilis incidunt nihil, rerum sunt tempora ut. Adipisci animi, architecto, deleniti est
+                exercitationem labore molestiae, molestias odio possimus quidem quis voluptas.</p>
+        </>
+    )
+}
+
+export default Home;
